0156fc12383d9075b96d0bba6c6400d1a3d26edf
tatsuaki_kin <tatuaki_dear@yahoo.co.jp>
2016-04-02 14:54:30 +0900

    add lib

--- app/src/main/java/memory/com/test/kin/my/testes/tes/memoryapp/MainActivitys.java
+++ app/src/main/java/memory/com/test/kin/my/testes/tes/memoryapp/MainActivitys.java
@@ -2,12 +2,19 @@ package memory.com.test.kin.my.testes.tes.memoryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivitys extends AppCompatActivity {
+    private ImageView imageView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activitys);
+
+        button = (Button) findViewById(R.id.button);
+        String packageName = BuildConfig.APPLICATION_ID;
     }
 }
